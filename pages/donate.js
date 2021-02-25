09af02e445a84bab01e4b733ffaf1114ad07b0f9
calincionca35 <36063669+calincionca35@users.noreply.github.com>
2021-02-25 11:34:36 -0800

    Update donate.js

--- pages/donate.js
+++ pages/donate.js
@@ -2,6 +2,7 @@ import Head from 'components/head';
 import Container from 'components/Container/Container';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
 import { s3 } from 'common/constants/urls';
+import Content from 'components/Content/Content';
 import styles from '../styles/donate.module.css';
 
 const pageTitle = 'Donate';
@@ -13,14 +14,27 @@ function DonatePage() {
 
       <HeroBanner title={pageTitle} />
 
+      <div>
+        <img
+          src={`${s3}guidestar_gold.png`}
+          alt="GuideStar Gold Transparency Seal"
+          className={styles.goldSealImg}
+        />
+      </div>
+
+      <Content
+        theme="white"
+        columns={[
+          <p>
+            PLACE HOLDER FOR DONATE PAGE COPY. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>,
+        ]}
+      />
+
       <Container theme="white">
-        <div>
-          <img
-            src={`${s3}guidestar_gold.png`}
-            alt="GuideStar Gold Transparency Seal"
-            className={styles.goldSealImg}
-          />
-        </div>
         <iframe
           title="Donation Form"
           src="https://secure.lglforms.com/form_engine/s/BRtP7QUKyHOyEYsZROsRew"
