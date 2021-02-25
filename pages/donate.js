35d951da9d45f6413e9d996c9066d4e877611009
calincionca35 <36063669+calincionca35@users.noreply.github.com>
2021-02-25 13:46:22 -0800

    Update donate.js

--- pages/donate.js
+++ pages/donate.js
@@ -1,5 +1,5 @@
 import Head from 'components/head';
-import Container from 'components/Container/Container';
+// import Container from 'components/Container/Container';
 import HeroBanner from 'components/HeroBanner/HeroBanner';
 import { s3 } from 'common/constants/urls';
 import Content from 'components/Content/Content';
@@ -17,21 +17,33 @@ function DonatePage() {
       <Content
         theme="white"
         columns={[
-          <img
-            src={`${s3}guidestar_gold.png`}
-            alt="GuideStar Gold Transparency Seal"
-            className={styles.goldSealImg}
-          />,
-          <p>
-            PLACE HOLDER FOR DONATE PAGE COPY. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-          </p>,
+          <div className={styles.donateContent}>
+            <img
+              src={`${s3}guidestar_gold.png`}
+              alt="GuideStar Gold Transparency Seal"
+              className={styles.goldSealImg}
+            />
+            <p>
+              PLACE HOLDER FOR DONATE PAGE COPY. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.
+            </p>
+            <iframe
+              title="Donation Form"
+              src="https://secure.lglforms.com/form_engine/s/BRtP7QUKyHOyEYsZROsRew"
+              height="1500px"
+              width="110%"
+              style={{
+                border: '1px solid transparent',
+                outline: '1px solid transparent',
+              }}
+            />
+          </div>,
         ]}
       />
 
-      <Container theme="white">
+      {/* <Container theme="white">
         <iframe
           title="Donation Form"
           src="https://secure.lglforms.com/form_engine/s/BRtP7QUKyHOyEYsZROsRew"
@@ -42,7 +54,7 @@ function DonatePage() {
             outline: '1px solid transparent',
           }}
         />
-      </Container>
+      </Container> */}
     </>
   );
 }
